d9f761d81d7419b6ff170e1904dfa130bd07b0e5
Nhan Nguyen <ntnhan0605@gmail.com>
2025-04-29 13:27:32 +0700

    Update CV

--- Javascript Advanced/01_js_to_know/08_Parameters_Arguments/main.js
+++
@@ -1,7 +0,0 @@
-var yearOfBirth = 1990;
-
-function calAge(year) {
-  return 2021 - year;
-}
-
-var age = calAge(yearOfBirth);
--- Javascript Advanced/04_iterators_generator_async-await/01/main.js
+++
@@ -1,8 +0,0 @@
-
-const listNumber = [40, 21, 53];
-
-for(let index = 0; index < listNumber.length; index++) {
-  let number = listNumber[index];
-  console.log('number', number);
-}
-
