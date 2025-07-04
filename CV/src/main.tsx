7ac9264a9585af7ce26fba6247377f5cff461c92
Nhan Nguyen <ntnhan0605@gmail.com>
2025-07-04 21:10:01 +0700

    CV

--- CV/src/main.tsx
+++ CV/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import * as react from 'react';
+import * as client from 'react-dom/client';
 import { CV } from './CV.tsx';
 import './main.scss';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+client.createRoot(document.getElementById('root')!).render(
+  <react.StrictMode>
     <CV />
-  </StrictMode>
+  </react.StrictMode>
 );
