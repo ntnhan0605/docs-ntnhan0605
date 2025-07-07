e647cb90694419bf6734e596b99e73b572a3818b
Nhan Nguyen <ntnhan0605@gmail.com>
2025-07-07 21:23:21 +0700

    chore-files - update files

--- Algorithm/BigO - Green 60/Lecture 01 - Practice/calculate_point.py
+++ Algorithm/BigO - Green 60/Lecture 01 - Practice/calculate_point.py
@@ -2,3 +2,24 @@ number = input()
 s = sum(int(n) for n in number)
 
 print(s % 10)
+
+
+# Another world -> Với input theo đề bài luôn luôn là số có 5 chữ số
+# Lấy chữ số cuối cùng của x (x5) -> x % 10
+# Lấy chữ số gần cuối của x (x4) -> (x // 10 ) % 10
+# ...
+# Lấy hết x1, x2, x3, x4, x5: (x1 + x2 + x3 + x4 + x5) % 10
+
+# x = int(input())
+# x5 = x % 10
+# x //= 10
+# x4 = x % 10
+# x //= 10
+# x3 = x % 10
+# x //= 10
+# x2 = x % 10
+# x //= 10
+# x1 = x % 10
+# x //= 10
+
+# print((x1 + x2 + x3 + x4 + x5) % 10)
