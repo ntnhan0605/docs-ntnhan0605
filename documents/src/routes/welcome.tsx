b27369e4118dd24b1328ee27dcb209355f9af26f
Nhan Nguyen <ntnhan0605@gmail.com>
2026-01-22 13:00:26 +0700

    [fixbug] - remove logo-light and logo-dark

--- documents/src/routes/welcome.tsx
+++ documents/src/routes/welcome.tsx
@@ -1,6 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
-import logoDark from '../../assets/logo-dark.svg'
-import logoLight from '../../assets/logo-light.svg'
 
 export const Route = createFileRoute('/welcome')({
   component: RouteComponent,
@@ -11,20 +9,20 @@ function RouteComponent() {
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
-          <div className="w-[500px] max-w-[100vw] p-4">
+          <div className="w-125 max-w-[100vw] p-4">
             <img
-              src={logoLight}
+              src="/logo-light.svg"
               alt="React Router"
               className="block w-full dark:hidden"
             />
             <img
-              src={logoDark}
+              src="/logo-dark.svg"
               alt="React Router"
               className="hidden w-full dark:block"
             />
           </div>
         </header>
-        <div className="max-w-[300px] w-full space-y-6 px-4">
+        <div className="max-w-75 w-full space-y-6 px-4">
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
             <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
               What&apos;s next?
